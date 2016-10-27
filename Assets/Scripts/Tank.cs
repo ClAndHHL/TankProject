436815ca4714e3310d24612352abd67575cdcf79
Lei Cao <Lei Cao>
2016-10-28 07:40:37 +0800

    Add Tank movement control

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : MonoBehaviour {
+
+    private float mSpeed = 5;
+    private Rigidbody mRigibid;
+
+	// Use this for initialization
+	void Start () {
+        mRigibid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float ver = Input.GetAxis("Vertical");
+        mRigibid.velocity = transform.forward * ver * mSpeed;
+    }
+}
